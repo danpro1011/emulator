24bac2046719b8fa2cd580b083c70430d8180c79
OfficialPeterZhao <peterzhaoofficial@gmail.com>
2026-03-03 20:59:13 -0600

    started implementation of main file with some methods

--- chip8.cpp
+++ chip8.cpp
@@ -1,5 +1,18 @@
 #include "chip8.hpp"
+#include <fstream>
 
-int main(int argc, char **argv) {
+Chip8::Chip8() {
+    PC = 0x200;
+}
 
+bool Chip8::loadRom(const std::string &filename) {
+
+}
+
+void Chip8::Cycle() {
+    // Fetch
+
+    // Decode
+
+    // Execute
 }
--- chip8.hpp
+++ chip8.hpp
@@ -4,29 +4,29 @@
 class Chip8 {
     public: 
     Chip8();
-    bool loadRom();
-    void Cycle();
+
+    bool loadRom(const std::string &filename); // want address of string file
+
+    void Cycle(); // fetch -> decode -> execute opcode
     
     
     private:
     // memory
-    uint8_t memory[4096];
+    uint8_t memory[4096]{};
     //registers
-    uint8_t reg[16];
+    uint8_t reg[16]{};
     //12 bit index register 
-    uint16_t addressRegister; // ignore 4 bits in this Register
+    uint16_t indexReg{}; // ignore 4 bits in this Register
     //program counter
-    uint16_t PC;
+    uint16_t PC = 0x200; // start at 0x200 below is reserved
     
     //stack
-    uint8_t sp = 0; //pointer
-    uint16_t stack[32]; // 48 bytes to 12 levels
+    uint8_t sp{}; //pointer
+    uint16_t stack[32]{}; // 48 bytes to 12 levels
 
-    uint8_t delayTimer;
-    uint8_t soundTimer;
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
 
     //0 to F "8", "4", "6", "2" directional input. array stores 0/1, 0 = not pressed, 1 = pressed
-    uint8_t input[16];
-
-
-}
+    uint8_t input[16]{};
+};
